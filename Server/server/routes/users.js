30611ccb939fe506fce14cbda0f9670a6ebc066e
joshi-aditya <joshi.adit@husky.neu.edu>
2018-04-25 14:47:01 -0400

    save user data

--- Server/server/routes/users.js
+++ Server/server/routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
+var bcrypt = require('bcryptjs');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* Creating users. */
+router.post('/', function(req, res, next) {
+
+    var user = new User({
+       firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: bcrypt.hashSync(req.body.password, 10),
+        email: req.body.email
+    });
+
+    user.save(function (err, result) {
+        if(err) {
+            return res.status(500).json({
+                title: 'Error while saving user',
+                error: err
+            });
+        }
+        res.status(201).json({
+            title: 'User created',
+            obj: result
+        });
+    });
 });
 
 module.exports = router;
